8e652c610cd7b29f27dfc3bc74c0f4ee3f34b058
julianzayas <julianzayas88@gmail.com>
2025-07-27 22:22:56 -0300

    inicio-version-2-2

--- config/constantes.php
+++ config/constantes.php
@@ -1,3 +1,3 @@
 <?php
-    define('BASE_URL', '/stockapp_v2.1'); // Cambiar si el nombre del proyecto es diferente
+    define('BASE_URL', '/stockapp_v2.2'); // Cambiar si el nombre del proyecto es diferente
 ?>
